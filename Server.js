bf3bb15d185bb5388594da7dd811af20f585f3a6
Delicatetheone2247 <58051883+Delicatetheone2247@users.noreply.github.com>
2020-09-25 18:25:17 +0200

    I have done the server but still facing some issues with the validation

---
+++ Server.js
@@ -0,0 +1,35 @@
+
+let http = require('http');
+let https = require('https');
+let fs = require('fs');
+
+//404 response
+function send404Response(response){
+  response.writeHead(404, {"Content-Type": "text/plain"});
+ response.write("Error 404: Page not found!");
+  response.end();
+}
+
+//Handle a user request
+function onRequest (request, response){
+
+  if( request.method == 'GET' && request.url == '/'){
+      response.writeHead(200, {"Content-Type": "text/html"});
+  fs.createReadStream("./index.html").pipe(response);
+  } else if (request.method == 'GET' && request.url == '/styles.css'){
+      response.writeHead(200, {"Content-Type": "text/css"});
+      fs.createReadStream('./styles.css').pipe(response);
+  } else if (request.method == 'GET' && request.url == '/Server.js') {
+      response.writeHead(200, {"Content-Type": "application/javascript"});
+      fs.createReadStream('./Server.js').pipe(response);
+  } else{
+      send404Response(response);
+  }
+}
+
+
+ //Starting  a server on  port
+http.createServer(onRequest).listen(8080);
+console.log ("server is running")
+
+
